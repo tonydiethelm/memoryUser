4af220b3e8c47de1bb0888f1be7d2dafb0dac812
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-09 14:27:20 -0800

    Changing to accept any amount of memory in Mb

--- express.js
+++ express.js
@@ -9,46 +9,14 @@ const port = 3002;
 app.use(express.json());
 
 
-app.get('/500', (request, response) => {
-    console.log("Ok, trying to burn 500Mb...");
-    const a = useMemory(500000000);
+app.get('/:memory', (request, response) => {
+    const memoryRequested = request.params.memory;
+    
+    
+    console.log(`Ok, trying to burn ${memoryRequested}Mb...`);
+    const a = useMemory(memoryRequest * 1000000);
     console.log(a[0]);
-    response.status(200).json('500Mb')});
-
-app.get('/1000', (request, response) => {
-    console.log("Ok, trying to burn 1Gb...");
-    const a = useMemory(500000000);
-    const b = useMemory(500000000);
-    console.log(a[0], b[0]);
-    response.status(200).json('1Gb')});
-
-app.get('/1500', (request, response) => {
-    console.log("Ok, trying to burn 1.5Gb...");
-    const a = useMemory(500000000);
-    const b = useMemory(500000000);
-    const c = useMemory(500000000);
-    console.log(a[0], b[0], c[0]);
-    response.status(200).json('1.5Gb')});    
-
-app.get('/2000', (request, response) => {
-    console.log("Ok, trying to burn 2Gb...");
-    const a = useMemory(500000000);
-    const b = useMemory(500000000);
-    const c = useMemory(500000000);
-    const d = useMemory(500000000);
-    console.log(a[0], b[0], c[0], d[0]);
-    response.status(200).json('2Gb')});    
-
-app.get('/2500', (request, response) => {
-    console.log("Ok, trying to burn 2.5Gb...");
-    const a = useMemory(500000000);
-    const b = useMemory(500000000);
-    const c = useMemory(500000000);
-    const d = useMemory(500000000);
-    const e = useMemory(500000000);
-    console.log(a[0], b[0], c[0], d[0], e[0]);
-    response.status(200).json('2.5Gb')});
-
+    response.status(200).json(`${memoryRequested}`)});
 
 app.listen(port, () => {
     console.log('Server listening on port: ' + port);
