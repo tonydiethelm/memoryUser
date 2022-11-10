cde386f93035fc7952a4f150dd7ae66bd3b3a483
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-10 09:28:23 -0800

    Further fixes

--- express.js
+++ express.js
@@ -7,17 +7,24 @@ const port = 3002;
 app.use(express.json());
 
 
+//use up memory for 3 mins.
 app.get('/:memory', (request, response) => {
-    //get memory requested
-    //send appropriate number to useMemory
-    //setTimeout to hold that big variable for 3 minutes, to make sure Prometheus/metrics sees it.
-    //Then console log from it, just so we use it somewhere, so it doesn't get garbage collected. 
+  //get memory requested
+  //send appropriate number to useMemory
+  //setTimeout to hold that big variable for 3 minutes, to make sure Prometheus/metrics sees it.
+  //Then console log from it, just so we use it somewhere, so it doesn't get garbage collected. 
 
-    const memoryRequested = request.params.memory;
-    console.log(`Ok, trying to burn ${memoryRequested}Mb...`);
-    const a = useMemory(memoryRequested * 1000000);
-    setTimeout(console.log(a[a.length - 1]), 180000);
-    response.status(200).json(`${memoryRequested}`)});
+  const memoryRequested = request.params.memory;
+  console.log(`Ok, trying to burn ${memoryRequested}Mb...`);
+  const a = useMemory(memoryRequested * 1000000);
+  setTimeout(() => {console.log(a[a.length - 1])}, 180000);
+  response.status(200).json(`${memoryRequested}`)});
+
+//Hello World
+app.get('/', (request, response) => {
+  response.status(200).send('Hello! Hola! Guten Tag! If you can see this, memory user is working. Send a GET to / with a number of Mb to use. IE: /200')});
+
+
 
 app.listen(port, () => {
     console.log('Server listening on port: ' + port);
